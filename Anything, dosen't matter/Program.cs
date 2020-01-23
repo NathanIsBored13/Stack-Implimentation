f8600d11f142b79de33705e71613a31b292ce560
Nathan Smith <K036659@eltham-college.org.uk>
2020-01-23 12:44:13 +0000

    Add project files.

---
+++ Anything, dosen't matter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Anything__dosen_t_matter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stack = new Stack<char>(7);
+            bool exit = false;
+            do
+            {
+                Console.Write("> ");
+                string[] input = Console.ReadLine().Split(" ");
+                switch (input[0])
+                {
+                    case "length":
+                        Console.WriteLine(stack.Length());
+                        break;
+                    case "push":
+                        stack.Push(input[1][0]);
+                        break;
+                    case "pop":
+                        Console.WriteLine(stack.Pop());
+                        break;
+                    case "peek":
+                        Console.WriteLine(stack.Peek());
+                        break;
+                    case "q":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("invalid command");
+                        break;
+                }
+                Console.WriteLine();
+            } while (!exit);
+        }
+    }
+}
---
+++ Anything, dosen't matter/Stack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Anything__dosen_t_matter
+{
+    class Stack<T>
+    {
+        private T[] stack;
+        private int ptr;
+        public Stack(int size)
+        {
+            stack = new T[size];
+        }
+
+        public int Length() => ptr;
+
+        public void Push(T value)
+        {
+            if (ptr < stack.Length) stack[ptr++] = value;
+            else Console.WriteLine("Stack is full");
+        }
+
+        public T Pop()
+        {
+            T ret = default;
+            if (ptr > 0) ret = stack[ptr-- - 1];
+            else Console.WriteLine("Stack is empty");
+            return ret;
+        }
+
+        public T Peek()
+        {
+
+            T ret = default;
+            if (ptr > 0) ret = stack[ptr - 1];
+            else Console.WriteLine("Stack is empty");
+            return ret;
+        }
+    }
+}
